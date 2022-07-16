19d22af8957d6e0f00ff1adeb2d4606fb7067739
AsteroidesINAOE <104033354+AsteroidesINAOE@users.noreply.github.com>
2022-07-16 13:09:20 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){    
+    $(".hide").hide();    
+        
+    $(".marker1").click(function(){    
+      $(".hide").hide();    
+      $(".txt1").fadeIn(300);    
+    });    
+            
+    $(".marker2").click(function(){    
+      $(".hide").hide();    
+      $(".txt2").fadeIn(300);    
+    });    
+            
+    $(".marker3").click(function(){    
+      $(".hide").hide();    
+      $(".txt3").fadeIn(300);    
+    });    
+            
+    $(".marker4").click(function(){    
+      $(".hide").hide();    
+      $(".txt4").fadeIn(300);    
+    });    
+            
+    $(".marker5").click(function(){    
+      $(".hide").hide();    
+      $(".txt5").fadeIn(300);    
+    });    
+            
+    $(".marker6").click(function(){    
+      $(".hide").hide();    
+      $(".txt6").fadeIn(300);    
+    });    
+            
+    $(".marker7").click(function(){    
+      $(".hide").hide();    
+      $(".txt7").fadeIn(300);    
+    });    
+            
+    $(".marker8").click(function(){    
+      $(".hide").hide();    
+      $(".txt8").fadeIn(300);    
+    });    
+            
+    $(".marker9").click(function(){    
+      $(".hide").hide();    
+      $(".txt9").fadeIn(300);    
+    });    
+            
+    $(".marker10").click(function(){    
+      $(".hide").hide();    
+      $(".txt10").fadeIn(300);    
+    });    
+    
+  });    
