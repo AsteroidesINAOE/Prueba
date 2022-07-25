2c3c26d6192e91f082edcaa6899dac70389c0509
AsteroidesINAOE <104033354+AsteroidesINAOE@users.noreply.github.com>
2022-07-24 19:34:09 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,54 +1,52 @@
-$(document).ready(function () {
-  $(".hide").hide();
+const grid = new Muuri('.grid', {
+	layout: {
+		rounding: false
+	}
+});
 
-  $(".marker1").click(function () {
-    $(".hide").hide();
-    $(".txt1").fadeIn(300);
-  });
+window.addEventListener('load', () => {
+	grid.refreshItems().layout();
+	document.getElementById('grid').classList.add('imagenes-cargadas');
 
-  $(".marker2").click(function () {
-    $(".hide").hide();
-    $(".txt2").fadeIn(300);
-  });
+	// Agregamos los listener de los enlaces para filtrar por categoria.
+	const enlaces = document.querySelectorAll('#categorias a');
+	enlaces.forEach((elemento) => {
+		elemento.addEventListener('click', (evento) => {
+			evento.preventDefault();
+			enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+			evento.target.classList.add('activo');
 
-  $(".marker3").click(function () {
-    $(".hide").hide();
-    $(".txt3").fadeIn(300);
-  });
+			const categoria = evento.target.innerHTML.toLowerCase();
+			categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+		});
+	});
 
-  $(".marker4").click(function () {
-    $(".hide").hide();
-    $(".txt4").fadeIn(300);
-  });
+	// Agregamos el listener para la barra de busqueda
+	document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+		const busqueda = evento.target.value;
+		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
+	});
 
-  $(".marker5").click(function () {
-    $(".hide").hide();
-    $(".txt5").fadeIn(300);
-  });
+	// Agregamos listener para las imagenes
+	const overlay = document.getElementById('overlay');
+	document.querySelectorAll('.grid .item img').forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const ruta = elemento.getAttribute('src');
+			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
 
-  $(".marker6").click(function () {
-    $(".hide").hide();
-    $(".txt6").fadeIn(300);
-  });
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+		});
+	});
 
-  $(".marker7").click(function () {
-    $(".hide").hide();
-    $(".txt7").fadeIn(300);
-  });
+	// Eventlistener del boton de cerrar
+	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+		overlay.classList.remove('activo');
+	});
 
-  $(".marker8").click(function () {
-    $(".hide").hide();
-    $(".txt8").fadeIn(300);
-  });
-
-  $(".marker9").click(function () {
-    $(".hide").hide();
-    $(".txt9").fadeIn(300);
-  });
-
-  $(".marker10").click(function () {
-    $(".hide").hide();
-    $(".txt10").fadeIn(300);
-  });
-
-});    
+	// Eventlistener del overlay
+	overlay.addEventListener('click', (evento) => {
+		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+	});
+});
